5fbacbe903fec09ed702bf735418d54f14b212f3
yaoze <yaoze@WILLSLITTLEBUDD>
2017-06-14 09:39:06 -0400

    Add project files.

---
+++ ContosoUniversity.Data/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ContosoUniversity.Model.Model;
+
+namespace ContosoUniversity.Data.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
